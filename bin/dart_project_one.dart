e5e6e26e3d21a9396c3197f8ff8fe9cc64ca27f9
rithik125 <rithikvr135@gmail.com>
2023-10-27 17:42:48 +0530

    first commit

---
+++ bin/dart_project_one.dart
@@ -0,0 +1,7 @@
+void main() {
+  print("my name is ambro");
+  print('i am 23 year old');
+  print('i am studying in luminar technolab');
+  print('my email id 20bam042.com');
+  print('my phone number is 66767787665');
+} 
---
+++ test/dart_project_one_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_project_one/dart_project_one.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
