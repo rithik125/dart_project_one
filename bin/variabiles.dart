accd7425a509548929f27fcae7f7689e0b298380
rithik125 <rithikvr135@gmail.com>
2023-11-03 18:44:02 +0530

    commit

---
+++ bin/variabiles.dart
@@ -0,0 +1,15 @@
+void main(){
+  String name = "rithik";
+  int age = 20;
+  String email = 'rithikvr135@gmail.com';
+  int phone = 12345678;
+  double cgpa = 7;
+  print(name);
+  print('my name is $name');
+  print('i am $age yer old');
+  print('my email id is $email');
+  print('my phone number is $phone');
+  print('my cgpa is $cgpa');
+  int a = 30 , b = 20;
+  print('total sum = ${a+b}');
+}
